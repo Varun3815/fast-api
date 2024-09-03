f0dc37ab6c61abeff62bccb3df769e1aba25b0bd
Varun3815 <varunkot@outlook.com>
2024-09-03 09:38:23 +0530

    wrangler uploaded

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       // once deployed, prefix this with your cloudflare worker url
       // i.e.: https://<name>.<account-name>.workers.dev/api/search?q=${input}
 
-      const res = await fetch(`/api/search?q=${input}`)
+      const res = await fetch(`https://fastapi.varunkot.workers.dev/api/search?q=${input}`)
       const data = (await res.json()) as { results: string[]; duration: number }
       setSearchResults(data)
     }
